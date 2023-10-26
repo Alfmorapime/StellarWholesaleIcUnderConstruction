371fcb8a9677394970d251d07ecf22d7556fdb72
Alfredo <alfmorapime@gmail.com>
2023-10-26 11:09:14 -0700

    Landing page file and components have been added

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include 'components/head.html'; ?>
+<body style="background:url(images/stellar_eblast_background.png)">
+    <!-- Body Inner -->
+    <div class="body-inner">
+        <?php
+            include 'components/header.html'; 
+            include 'components/content.html'; 
+            include 'components/footer.html'; 
+        ?>
+    </div> 
+    <!-- end: Body Inner -->
+    <?php include 'components/js-links.html'; ?>
+</body>
+</html>
